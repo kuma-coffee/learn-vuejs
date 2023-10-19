1726429bddb91b82d31a9fdced2be362817b87e8
kuma-coffee <daniel099d@gmail.com>
2023-10-20 03:27:31 +0700

    add lesson-4

--- lesson-4/modal-project/src/App.vue
+++ lesson-4/modal-project/src/App.vue
@@ -1,45 +1,56 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <p>Welcome...</p>
-    <div v-if="showModal">
-      <Modal theme="sale" @close="toggleModal">
-        <template v-slot:links>
-          <a href="#">sign up now</a>
-          <a href="#">more info</a>
-        </template>
-        <h1>Ninja Giveaway!</h1>
-        <p>Grab your ninja swag for half price!</p>
-      </Modal>
-    </div>
-    <button @click.alt="toggleModal">open modal (alt)</button>
-  </div>
+  <h1>{{ title }}</h1>
+  <p>Welcome...</p>
+
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Ninja Givaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+      <h1>Sign up to the Newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+
+  <button @click.alt="toggleModal">open modal (alt click)</button>
+  <button @click="toggleModalTwo">open modal 2</button>
 </template>
 
 <script>
-import Modal from "./components/Modal.vue";
+import Modal from "./components/Modal";
 
 export default {
   name: "App",
   components: { Modal },
   data() {
     return {
-      title: "My First Vue App :)",
-      header: "Sign up for the Giveaway!",
-      text: "Grab your ninja swag for half price!",
+      title: "My First Vue App!",
       showModal: false,
+      showModalTwo: false,
     };
   },
   methods: {
     toggleModal() {
       this.showModal = !this.showModal;
     },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
   },
 };
 </script>
 
 <style>
-#app {
+#app,
+#modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
