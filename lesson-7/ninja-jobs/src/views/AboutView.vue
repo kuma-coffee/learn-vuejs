74047cc2e7fd96a21a0f3378ae9e4f5a88d2c5a8
kuma-coffee <daniel099d@gmail.com>
2023-10-25 00:00:59 +0700

    add lesson-7

---
+++ lesson-7/ninja-jobs/src/views/AboutView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat modi
+      quisquam dicta minus consequuntur repudiandae, similique ut ipsam harum
+      eum a vel sint mollitia veritatis voluptatem architecto quae, explicabo
+      pariatur reprehenderit amet hic debitis provident nobis. Ea nisi earum
+      exercitationem, qui perspiciatis voluptate dolor aperiam quaerat iusto
+      sint, dolores, tenetur dicta saepe ut beatae quo voluptatibus architecto
+      totam! Fuga doloremque sapiente exercitationem atque quis temporibus
+      quaerat quod et aspernatur laudantium, itaque, recusandae tempora vel.
+      Laboriosam libero, nostrum aspernatur officia, quis voluptatum voluptates
+      reprehenderit eligendi provident ea optio ipsa accusamus illum assumenda
+      possimus cum mollitia unde fugiat atque sunt, ipsum vero?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat modi
+      quisquam dicta minus consequuntur repudiandae, similique ut ipsam harum
+      eum a vel sint mollitia veritatis voluptatem architecto quae, explicabo
+      pariatur reprehenderit amet hic debitis provident nobis. Ea nisi earum
+      exercitationem, qui perspiciatis voluptate dolor aperiam quaerat iusto
+      sint, dolores, tenetur dicta saepe ut beatae quo voluptatibus architecto
+      totam! Fuga doloremque sapiente exercitationem atque quis temporibus
+      quaerat quod et aspernatur laudantium, itaque, recusandae tempora vel.
+      Laboriosam libero, nostrum aspernatur officia, quis voluptatum voluptates
+      reprehenderit eligendi provident ea optio ipsa accusamus illum assumenda
+      possimus cum mollitia unde fugiat atque sunt, ipsum vero?
+    </p>
+  </div>
+</template>
