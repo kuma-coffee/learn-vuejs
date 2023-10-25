8cb5798731139af2eacd85ebacb6bc30a7bde36e
kuma-coffee <daniel099d@gmail.com>
2023-10-25 09:21:30 +0700

    add lesson 9

---
+++ lesson-9/dojo-blog/src/views/Details.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <h1>Details</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="post">
+      <h3>{{ post.title }}</h3>
+      <p class="pre">{{ post.body }}</p>
+    </div>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+import getPost from "../composables/GetPost";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    const { post, error, load } = getPost(props.id);
+
+    load();
+
+    return { post, error };
+  },
+};
+</script>
+
+<style>
+.post {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.post p {
+  color: #444;
+  line-height: 1.5em;
+  margin-top: 40px;
+}
+
+.pre {
+  white-space: pre-wrap;
+}
+</style>
---
+++ lesson-9/dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <!-- <PostList v-if="showPosts" :posts="posts" />
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button> -->
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+  </div>
+</template>
+
+<script>
+import PostList from "../components/PostList.vue";
+import getPosts from "../composables/GetPosts";
+
+export default {
+  name: "HomeView",
+  components: { PostList },
+  setup() {
+    // const showPosts = ref(true);
+
+    const { posts, error, load } = getPosts();
+
+    load();
+
+    return { posts, error };
+  },
+};
+</script>
